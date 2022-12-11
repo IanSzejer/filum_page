5d9cb44101f84a0315147ffa44870e8ffe2829be
mateoperezrivera <mateoperezrivera@gmail.com>
2022-12-11 13:33:25 -0300

    Primera versoin basica de pagina

---
+++ src/components/FooterBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-footer color="#eee8dd" padless outlined class="mt-3 footer" >
+    <v-card-text class="flex text-center text">
+      <div class="W-ubication-div">
+        <img src="@/assets/location.png" class="W-ubication-logo" alt="Instagram Logo"/>
+        <label class="W-ubication-label"> Vicente Lopez, BsAs</label>
+      </div>
+      {{new Date().getFullYear()}} - Filum
+    </v-card-text>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "FooterBar"
+}
+</script>
+
+<style scoped>
+.text{
+  font-weight: bold;
+  font-size: 15px;
+}
+.footer{
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
+
+.W-ubication-label{
+  text-align: center;
+}
+
+.W-ubication-div{
+  display: flex;
+  justify-content: center;
+}
+
+.W-ubication-logo{
+
+  width: 20px;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,72 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-card class="mx-auto my-12 W-card-style"
+            max-width="40%">
+      <v-card-text>
+        <p class="text-h4 text--primary W-tittle">
+          Somos Filum
+        </p>
+          <div class="W-link-div">
+            <a href="https://www.w3schools.com">
+              <img src="@/assets/instagram.jpg" class="image-style" alt="Instagram Logo"/>
+              <label class="image-label">@Filum_3D</label>
+            </a>
+          </div>
+        <div class="W-link-div">
+          <a href="https://www.w3schools.com">
+            <img src="@/assets/whattsap.png" class="image-style" alt="Whatsapp Logo"/>
+            <label class="image-label">+54 11 2190-9692</label>
+          </a>
+        </div>
+        <div class="W-link-div">
+          <a href="https://www.w3schools.com">
+            <img src="@/assets/mail.png" class="image-style" alt="Mail Logo"/>
+            <label class="image-label">info@filum3d.com</label>
+          </a>
+        </div>
+      </v-card-text>
+
+
+
+    </v-card>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  export default {
+    name: 'HelloWorld',
+
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+.W-card-style{
+  display: flex;
+  justify-content: left;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.W-tittle{
+  margin-left: 60px;
+
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.W-link-div{
+  display: flex;
+  text-align: left;
+  margin: 5px;
+
 }
-a {
-  color: #42b983;
+
+.image-style{
+  max-width: 7%;
+  vertical-align:middle;
+  margin-left: 60px;
+}
+
+
+.image-label{
+  padding-left: 5px;
+  vertical-align:middle;
 }
 </style>
